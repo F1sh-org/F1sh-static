c2babc6ef383d088b31ed075c792cca55cabe9c0
b4iterdev <thaimeo200389@gmail.com>
2025-03-11 14:04:30 +0700

    add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ControllerComponent } from './controller/controller.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/controller', pathMatch: 'full' },
-  { path: 'controller', component: ControllerComponent }
+  { path: 'controller', component: ControllerComponent },
+  { path: 'config', component: ConfigurationComponent }
 ];
 
 @NgModule({
